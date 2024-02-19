7f349caaf13fdef50cbba26f708120440a7f9c7f
Nikita <101108890+nvadeveloper@users.noreply.github.com>
2024-02-19 23:22:58 +0300

    added step by step form

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { type Context, Markup, Telegraf, Telegram } from 'telegraf';
+import { type Context, Markup, Telegraf, Telegram, Scenes, Composer, session } from 'telegraf';
 import express from 'express';
 import * as dotenv from 'dotenv';
 import { СurrencyBackendType, СurrencyType } from './types';
@@ -9,114 +9,61 @@ dotenv.config();
 const notion = new Client({ auth: process.env.NOTION_API_KEY });
 const databaseId = process.env.NOTION_DATABASE_ID;
 
-const CURRENCY_MAP = new Map<string, string>([
-    ['USD000UTSTOM', '🇺🇸 USD'],
-    ['EUR_RUB__TOM', '🇪🇺 EUR'],
-    ['CNYRUB_TOM', '🇨🇳 CNY'],
-    ['TRYRUB_TOM', '🇹🇷 TRY'],
-    ['HKDRUB_TOM', '🇭🇰 HKD'],
-    ['KZTRUB_TOM', '🇵🇼 KZT'],
-]);
-
 const token: string = process.env.BOT_TOKEN ?? '';
 const port: string = process.env.PORT ?? '';
 const url: string = process.env.URL ?? '';
-
 const bot = new Telegraf<Context<any>>(token);
 
+const startWizardScene = new Composer<Scenes.WizardContext>();
+const nameWizardScene = new Composer<Scenes.WizardContext>();
+const emailWizardScene = new Composer<Scenes.WizardContext>();
+const phoneWizardScene = new Composer<Scenes.WizardContext>();
+
+startWizardScene.on('text', async (ctx) => {
+    await ctx.reply('Введите имя и фамилию');
+
+    return ctx.wizard.next();
+});
+
+nameWizardScene.on('text', async (ctx) => {
+    await ctx.reply('Введите email');
+    return ctx.wizard.next();
+});
+
+emailWizardScene.on('text', async (ctx) => {
+    await ctx.reply('Введите номер телефона');
+    return ctx.wizard.next();
+});
+
+phoneWizardScene.on('text', async (ctx) => {
+    await ctx.reply('Спасибо анкета заполнена!');
+
+    return ctx.scene.leave();
+});
+
+const menuScene = new Scenes.WizardScene(
+    'wizardScene',
+    startWizardScene,
+    nameWizardScene,
+    emailWizardScene,
+    phoneWizardScene,
+);
+const stage = new Scenes.Stage([menuScene] as any);
+
 const app = express();
+
+bot.use(session());
+bot.use(stage.middleware() as any);
 bot.launch();
-
-async function addToDatabase(databaseId: any, username: string, date: any) {
-    try {
-        const response = await notion.pages.create({
-            parent: {
-                database_id: databaseId,
-            },
-            properties: {
-                id: {
-                    type: 'title',
-                    title: [
-                        {
-                            type: 'text',
-                            text: {
-                                content: username,
-                            },
-                        },
-                    ],
-                },
-                date: {
-                    type: 'rich_text',
-                    rich_text: [
-                        {
-                            type: 'text',
-                            text: {
-                                content: getCurrentDate(),
-                            },
-                        },
-                    ],
-                },
-                // email: {
-                //     type: 'email',
-                //     email: 'nik@nik.ru',
-                //     // type: 'email',
-                //     // email: [
-                //     //     {
-                //     //         type: 'email',
-                //     //         text: {
-                //     //             content: getCurrentDate(),
-                //     //         },
-                //     //     },
-                //     // ],
-                // },
-            },
-        });
-        console.log(response);
-    } catch (error) {
-        console.error(error);
-    }
-}
-
-const filterData = (data: СurrencyBackendType[]) => {
-    const array: СurrencyType[] = [];
-
-    data.filter((item: СurrencyBackendType) => {
-        if (CURRENCY_MAP.get(item.SECID)) {
-            array.push({
-                text: CURRENCY_MAP.get(item.SECID) ?? '',
-                value: item.LAST,
-                time: item.UPDATETIME,
-            });
-        }
-    });
-
-    return array;
-};
-
-const convertToText = (data: СurrencyType[]) =>
-    data.map((item) => `${item.text} ${item.value}. Обновлено: ${item.time}`);
 
 app.listen(port, () => {
     console.log(`Dolphin app listening on port ${port}!`);
     bot.start(async (ctx) => {
         await ctx.reply('👋');
         await ctx.reply('Привет, ' + ctx.from.first_name + '!');
-        await ctx.reply('Заполни анкету, сначала введи имя');
+        // @ts-ignore
+        await ctx.scene.enter('wizardScene');
     });
-
-    // bot.start(async (ctx) => {
-    //     await ctx.reply('Привет, ' + ctx.from.first_name + '!', {
-    //         ...Markup.inlineKeyboard([Markup.button.callback('Заполнить анкету', 'currency')]),
-    //     });
-    // });
-
-    // bot.action('currency', async (ctx) => {
-    //     const data = await fetch(url).then((data) => data.json());
-    //     await addToDatabase(databaseId, String(ctx.from?.id), Date.now());
-    //     await ctx.reply(convertToText(filterData(data[1].marketdata)).join('\n'), {
-    //         ...Markup.inlineKeyboard([Markup.button.callback('Обновить курс валют', 'currency')]),
-    //     });
-    // });
 });
 
 app.get('/', (req, res) => {
--- src/utils.ts
+++ src/utils.ts
@@ -13,3 +13,67 @@ export function getCurrentDate() {
         date.getFullYear()
     }`;
 }
+
+// async function addToDatabase(databaseId: any, username: string) {
+//     try {
+//         const response = await notion.pages.create({
+//             parent: {
+//                 database_id: databaseId,
+//             },
+//             properties: {
+//                 id: {
+//                     type: 'title',
+//                     title: [
+//                         {
+//                             type: 'text',
+//                             text: {
+//                                 content: username,
+//                             },
+//                         },
+//                     ],
+//                 },
+//                 date: {
+//                     type: 'rich_text',
+//                     rich_text: [
+//                         {
+//                             type: 'text',
+//                             text: {
+//                                 content: getCurrentDate(),
+//                             },
+//                         },
+//                     ],
+//                 },
+//                 // email: {
+//                 //     type: 'email',
+//                 //     email: 'nik@nik.ru',
+//                 //     // type: 'email',
+//                 //     // email: [
+//                 //     //     {
+//                 //     //         type: 'email',
+//                 //     //         text: {
+//                 //     //             content: getCurrentDate(),
+//                 //     //         },
+//                 //     //     },
+//                 //     // ],
+//                 // },
+//             },
+//         });
+//         console.log(response);
+//     } catch (error) {
+//         console.error(error);
+//     }
+// }
+
+// bot.start(async (ctx) => {
+//     await ctx.reply('Привет, ' + ctx.from.first_name + '!', {
+//         ...Markup.inlineKeyboard([Markup.button.callback('Заполнить анкету', 'currency')]),
+//     });
+// });
+
+// bot.action('currency', async (ctx) => {
+//     const data = await fetch(url).then((data) => data.json());
+//     await addToDatabase(databaseId, String(ctx.from?.id), Date.now());
+//     await ctx.reply(convertToText(filterData(data[1].marketdata)).join('\n'), {
+//         ...Markup.inlineKeyboard([Markup.button.callback('Обновить курс валют', 'currency')]),
+//     });
+// });
